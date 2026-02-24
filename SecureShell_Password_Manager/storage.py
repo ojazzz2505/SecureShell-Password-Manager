e621fc8eb97fe5303b4886733348f753b7c69cf9
Kyle Rutland <krutland@augusta.edu>
2026-02-24 16:00:29 -0500

    Global settings and load_data() completed

--- SecureShell_Password_Manager/storage.py
+++ SecureShell_Password_Manager/storage.py
@@ -16,6 +16,10 @@ from pathlib import Path
 # 3. Define the path for the database file (e.g., 'passwords.json').
 # DB_FILE = ...
 
+GLOBAL_APP_DIR = Path.home() / ".spm_data"
+if not Path(GLOBAL_APP_DIR).exists:
+    os.system("mkdir GLOBAL_APP_DIR")
+DB_FILE = Path.home() / "passwords.json"
 
 # ==========================================
 # FUNCTIONS
@@ -26,7 +30,10 @@ def load_data():
     # 1. Check if DB_FILE exists.
     # 2. If it does, open it and use json.load() to return the dictionary.
     # 3. If it doesn't, return an empty dictionary {}.
-    return {}
+    if Path("passwords.json").exists():
+        file = open("passwords.json")
+        return json.load(file)
+    return "passwords.json does not exist."
 
 def save_data(data):
     # TODO:
